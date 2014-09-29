c90ea994d3ca60154a8dcdbd7f2878c7c3da42a2
Andreas Wallner <A.Wallner@innovative-solutions.at>
2014-09-29 21:35:47 +0200

    more flexible tables, prep for polygons

--- bit.py
+++ bit.py
@@ -6,56 +6,62 @@ from MatplotlibSurface import *
 
 
 class Instruction(object):
-    def __init__(self, icode, width):
-        self.width = width
-        self.icode = icode
+  def __init__(self, icode, width):
+    self.width = width
+    self.icode = icode
 
-    def symbol(self, painter):
-        return self.icode
+  def symbol(self, painter):
+    return self.icode
 
-    def end(self, painter):
-        return (self.width * painter.env.bitwidth, 0)
+  def end(self, painter):
+    return (self.width * painter.env.bitwidth, 0)
 
-    def at_start(self, anchor, painter):
-        return anchors[anchor](self.width, painter.env)
+  def at_start(self, anchor, painter):
+    return anchor(self.width, painter.env)
 
-    def at_end(self, anchor, painter):
-        offset = anchors[anchor](self.width, painter.env)
-        return OffsetCoord(self.width * painter.env.bitwidth, 0) - offset
+  def at_end(self, anchor, painter):
+    offset = anchor(self.width, painter.env)
+    return OffsetCoord(self.width * painter.env.bitwidth, 0) - offset
 
-    def render(self, painter):
-        self.render_transition(painter)
-        if self.width != 0:
-            self.render_symbol(painter)
+  def render(self, painter):
+    self.render_transition(painter)
+    if self.width != 0:
+      self.render_symbol(painter)
+      self.render_background(painter)
 
-    def render_symbol(self, painter):
-        s = self.symbol(painter)
-        
-        lines = symbols[s]
-        for line in lines:
-            c1 = self.at_start(line[0], painter)
-            c2 = self.at_end(line[1], painter)
-            print(c1)
-            print(c2)
-            painter.draw_line(c1, c2)
+  def render_symbol(self, painter):
+    s = self.symbol(painter)
+    
+    lines = symbols[s]
+    for line in lines:
+      c1 = self.at_start(line[0], painter)
+      c2 = self.at_end(line[1], painter)
+      painter.draw_line(c1, c2)
 
-    def render_transition(self, painter):
-        transition = transitions[(painter.last_symbol, self.symbol(painter))]
+  def render_background(self, painter):
+    s = self.symbol(painter)
+    
+    polygons = backgrounds[s]
+    for polygon in polygons:
+      pass
+      
+  def render_transition(self, painter):
+    transition = transitions[(painter.last_symbol, self.symbol(painter))]
 
-        for line in transition:
-            c1 = -anchors[line[0]](0, painter.env)
-            c2 = anchors[line[1]](0, painter.env)
-            painter.draw_line(c1, c2)
+    for line in transition:
+      c1 = -line[0](0, painter.env)
+      c2 = line[1](0, painter.env)
+      painter.draw_line(c1, c2)
 
 class SimpleSequence(object):
-    def __init__(self, symbols, env):
-        self.chain = [Origin((0,0), 'S', env)]
-        for symbol in symbols:
-            new = BasicBit(symbol, 1, self.chain[-1], env)
-            self.chain.append(new)
-        self.chain.append(End('S', self.chain[-1], env))
+  def __init__(self, symbols, env):
+    self.chain = [Origin((0,0), 'S', env)]
+    for symbol in symbols:
+      new = BasicBit(symbol, 1, self.chain[-1], env)
+      self.chain.append(new)
+    self.chain.append(End('S', self.chain[-1], env))
 
-    def render(self, surface):
-        for i in range(len(self.chain) - 1):
-            self.chain[i].render(surface, self.chain[i+1])
+  def render(self, surface):
+    for i in range(len(self.chain) - 1):
+      self.chain[i].render(surface, self.chain[i+1])
 
--- tables.py
+++ tables.py
@@ -1,30 +1,60 @@
 from OffsetCoord import *
 
-anchors = {
-    'ea' : (lambda width, env : OffsetCoord(0, env.lineheight / 2)),
-    'ec' : (lambda width, env : OffsetCoord(0, 0)),
-    'eb' : (lambda width, env : OffsetCoord(0, -env.lineheight / 2)),
-    'ba' : (lambda width, env : OffsetCoord(env.lineheight * env.slope * 0.5, env.lineheight / 2)),
-    'bc' : (lambda width, env : OffsetCoord(env.lineheight * env.slope * 0.5, 0)),
-    'bb' : (lambda width, env : OffsetCoord(env.lineheight * env.slope * 0.5, -env.lineheight / 2)),
-    'ca' : (lambda width, env : OffsetCoord(width * env.bitwidth, env.lineheight / 2)),
-    'cc' : (lambda width, env : OffsetCoord(width * env.bitwidth, 0)),
-    'cb' : (lambda width, env : OffsetCoord(width * env.bitwidth, -env.lineheight / 2)),
+def lsum(l1, l2):
+  return (lambda width, env: l1(width, env) + l2(width, env))
+ 
+def lend(l):
+  return (lambda width, env: OffsetCoord(width * env.bitwidth, 0) + l1(width, env).flipLR())
+ 
+def lleft(l):
+  return (lambda width, env: l(width, env).flipLR())
+ 
+def lright(l):
+  return (lambda width, env: l(width, env))
+
+def combinate(d1, d2):
+  result = {}
+  for foo in d1:
+    for bar in d2:
+      result[foo + bar] = lsum(d1[foo], d2[bar])
+  return result
+
+y_offsets = {
+  'c' : (lambda width, env: OffsetCoord(0, 0)),
+  'b' : (lambda width, env: OffsetCoord(0, -env.lineheight / 2)),
+  'a' : (lambda width, env: OffsetCoord(0, env.lineheight / 2)),
+}
+ 
+x_offsets = {
+  'e' : (lambda width, env: OffsetCoord(0, 0)),
+  's' : (lambda width, env: OffsetCoord(env.lineheight * env.slope, 0)),
+  'm' : (lambda width, env: OffsetCoord(env.lineheight * env.slope / 2, 0)),
+  'c' : (lambda width, env: OffsetCoord(width * env.bitwidth / 2, 0)),
 }
 
+anchors = combinate(x_offsets, y_offsets)
+locals().update({'a_' + x : anchors[x] for x in anchors})
+
 symbols = {
-    'S' : [],
-    'L' : [('bb', 'bb')],
-    'H' : [('ba', 'ba')],
+  'S' : [],
+  'L' : [(a_sb, a_sb)],
+  'H' : [(a_sa, a_sa)],
+  'U' : [(a_sb, a_sb), (a_sa, a_sa)],
 }
 
 transitions = {
-    ('S', 'L') : [('eb', 'bb')],
-    ('S', 'H') : [('ea', 'ba')],
-    ('L', 'S') : [('bb', 'eb')],
-    ('H', 'S') : [('ba', 'ea')],
-    ('L', 'H') : [('bb', 'ba')],
-    ('L', 'L') : [('bb', 'bb')],
-    ('H', 'H') : [('ba', 'ba')],
-    ('H', 'L') : [('ba', 'bb')],
+  ('S', 'L') : [(a_eb, a_sb)],
+  ('S', 'H') : [(a_ea, a_sa)],
+  ('L', 'S') : [(a_sb, a_eb)],
+  ('H', 'S') : [(a_sa, a_ea)],
+  ('L', 'H') : [(a_sb, a_sa)],
+  ('L', 'L') : [(a_sb, a_sb)],
+  ('H', 'H') : [(a_sa, a_sa)],
+  ('H', 'L') : [(a_sa, a_sb)],
+}
+
+backgrounds = {
+  'U' : [],
+  'L' : [],
+  'H' : [],
 }
