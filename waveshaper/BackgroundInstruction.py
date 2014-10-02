490155aca0908a086141bee78f4c281043c5096d
Andreas Wallner <A.Wallner@innovative-solutions.at>
2014-10-02 22:16:48 +0200

    added comparison func to BackgroundInstruction

--- waveshaper/BackgroundInstruction.py
+++ waveshaper/BackgroundInstruction.py
@@ -7,6 +7,14 @@ class BackgroundInstruction(object):
   def __repr__(self):
     return 'BackgroundInstruction({0!r})'.format(self.seq)
 
+  def __eq__(self, other):
+    if isinstance(other, self.__class__):
+      return hasattr(other, '__dict__') and self.__dict__ == other.__dict__
+    return NotImplemented
+
+  def __ne__(self, other):
+    return not self.__eq__(other)
+
   def execute(self, painter):
     origEnv = copy(painter.env)
     pos = painter.pos
---
+++ waveshaper/test_BackgroundInstruction.py
@@ -0,0 +1,23 @@
+import unittest
+from BackgroundInstruction import BackgroundInstruction
+from RenderInstruction import RenderInstruction
+from utils import RealEqualMixin
+
+class tests(unittest.TestCase, RealEqualMixin):
+  def test_eq(self):
+    self.assertRealEqual(
+      BackgroundInstruction(None),
+      BackgroundInstruction(None))
+
+    self.assertRealEqual(
+      BackgroundInstruction([RenderInstruction('L', 1)]),
+      BackgroundInstruction([RenderInstruction('L', 1)]))
+
+    self.assertRealNotEqual(
+      BackgroundInstruction([RenderInstruction('L', 1)]),
+      BackgroundInstruction([RenderInstruction('H', 1)]))
+
+    self.assertRealNotEqual(
+      BackgroundInstruction([RenderInstruction('L', 1)]),
+      BackgroundInstruction(None))
+
