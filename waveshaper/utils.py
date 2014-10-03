ebb82d93d3614072e115aaebe5a90f91b393664c
Andreas Wallner <A.Wallner@innovative-solutions.at>
2014-10-04 00:08:42 +0200

    much better argument output from utils.Trace

--- waveshaper/utils.py
+++ waveshaper/utils.py
@@ -86,7 +86,10 @@ def Trace(f):
     @wraps(f)
     def dec_f(*args, **kwargs):
         print('entering ' +  f.__name__)
-        print('  args: ' + str(args))
+        print('  args: ')
+        for arg in args:
+          print('    ' + str(arg))
+          print('')
         print('  kwargs: ' + str(kwargs))
         result = f(*args, **kwargs)
         print('  returns {}'.format(str(result)))
