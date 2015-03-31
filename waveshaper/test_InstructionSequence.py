d417781196140e9f39d30bb5469edece9ae15c06
Andreas Wallner <A.Wallner@innovative-solutions.at>
2015-03-31 23:50:12 +0200

    fixed InstructionSequence tests

--- waveshaper/test_InstructionSequence.py
+++ waveshaper/test_InstructionSequence.py
@@ -1,5 +1,7 @@
 import unittest
+from copy import copy
 from .InstructionSequence import InstructionSequence
+from .EnvironmentChangeInstruction import EnvironmentChangeInstruction
 from .utils import RealEqualMixin
 
 class tests(unittest.TestCase, RealEqualMixin):
@@ -25,7 +27,7 @@ class tests(unittest.TestCase, RealEqualMixin):
       InstructionSequence(['foo'], 2))
 
   def test_execute_single(self):
-    seq = [dummyInstr(), dummyInstr(), dummyInstr()]
+    seq = [dummyInstr(1), dummyInstr(2), dummyInstr(3)]
     p = dummyPainter()
 
     inseq = InstructionSequence(seq, 1)
@@ -35,26 +37,73 @@ class tests(unittest.TestCase, RealEqualMixin):
       self.assertEqual(s.callcnt, 1)
       self.assertEqual(s.painter, p)
 
+    self.assertEqual(p.painted, [1,2,3])
+
   def test_execute_multiple(self):
-    seq = [dummyInstr(), dummyInstr(), dummyInstr()]
+    seq = [dummyInstr(1), dummyInstr(2)]
     p = dummyPainter()
 
-    inseq = InstructionSequence(seq, 5)
+    inseq = InstructionSequence(seq, 2)
     inseq.execute(p)
 
     for s in seq:
-      self.assertEqual(s.callcnt, 5)
+      self.assertEqual(s.callcnt, 2)
       self.assertEqual(s.painter, p)
+
+    self.assertEqual(p.painted, [1,2,1,2])
+
+  def test_envStack(self):
+    seq = [
+      dummyInstr(1),
+      EnvironmentChangeInstruction('test', 2),
+      dummyInstr(2),
+      ]
+    p = dummyPainter()
+
+    inseq = InstructionSequence(seq, 1)
+    inseq.execute(p)
+
+    for s in seq:
+      if type(s) is dummyInstr:
+        self.assertEqual(s.callcnt, 1)
+        self.assertEqual(s.painter, p)
+
+    self.assertEqual(p.painted, [1,2])
+    self.assertEqual(
+      p.paintedEnv,
+      [
+        {'test' : 1},
+        {'test' : 2},
+        ])
+
+  def test_xy(self):
+    # 
+    pass
     
 class dummyInstr(object):
-  def __init__(self):
+  def __init__(self, name):
+    self.name = name
     self.callcnt = 0
     self.painter = None
 
   def execute(self, painter):
     self.callcnt += 1
     self.painter = painter
+    painter.paint(self.name, painter.env)
 
 class dummyPainter(object):
   def __init__(self):
-    self.env = None
+    self.env = {'test' : 1}
+    self.pushed = []
+    self.painted = []
+    self.paintedEnv = []
+
+  def paint(self, name, env):
+    self.painted.append(name)
+    self.paintedEnv.append(copy(env))
+
+  def pushEnv(self):
+    self.pushed.append(copy(self.env))
+
+  def popEnv(self):
+    self.env = self.pushed.pop()
